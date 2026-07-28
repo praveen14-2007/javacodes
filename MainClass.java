47a5f24fcbe70cd415ef735412f62466304fa762
Praveen kumar <pk0173306@gmail.com>
2026-07-28 21:03:21 +0530

    Add MainClass with basic student information display

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+class MainClass{
+    public static void main(String args[]){
+        String name = "praveen";
+        int age = 19;
+        int roll = 450;
+        int maths_marks = 90;
+        int science_marks = 80;
+        int sum = maths_marks + science_marks;
+        System.out.println(maths_marks);
+        System.out.println(science_marks);
+        System.out.println(sum);
+    }
+}
