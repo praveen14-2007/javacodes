8da1d32692fa50d632faa010273165beb79becdf
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:40:07 +0530

    Add FifthStringsClass to demonstrate substring usage

---
+++ FifthStringsClass.java
@@ -0,0 +1,7 @@
+class FifthStringsClass{
+    public static void main(String[] args){
+        String fname = "praveen";
+        String sname = fname.substring(4);
+        System.out.println(sname);
+    }
+}
