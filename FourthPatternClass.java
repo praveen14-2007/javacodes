e9b9ebf9e2096aa01bfa99aff877b4ac96d8db80
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:21:21 +0530

    Add FourthPatternClass to print star pattern

---
+++ FourthPatternClass.java
@@ -0,0 +1,10 @@
+class FourthPatternClass{
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++){ //for(int i=4; i>=1;i--)
+            for(int j=4;j>=i;j--){ //for(int j=1;j<=i;j++)
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
