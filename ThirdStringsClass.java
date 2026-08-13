84e220df3bd6dd0629cb9071b1d9168a04344de0
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:39:35 +0530

    Add ThirdStringsClass to compare two strings

---
+++ ThirdStringsClass.java
@@ -0,0 +1,12 @@
+class ThirdStringsClass{
+    public static void main(String[] args){
+        String name1="praveen";
+        String name2="praveeen";
+        if(name1.compareTo(name2)==0){
+            System.out.println("Strings are equal");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
