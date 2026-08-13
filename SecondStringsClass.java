d3f298b4781831454b25451cadbf8bd7cde96058
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:39:02 +0530

    Add SecondStringsClass to concatenate and print strings

---
+++ SecondStringsClass.java
@@ -0,0 +1,11 @@
+class SecondStringsClass{
+    public static void main(String[] args){
+        String name1="praveen";
+        String name2="kumar";
+        String name=name1 + name2;
+        System.out.println(name.length());
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
