c611b2c62098b8e5f540d830652c3898b6485851
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:46:01 +0530

    Implement bit manipulation in ThirdBitClass

---
+++ ThirdBitClass.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ThirdBitClass{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int oper=sc.nextInt();
+        int n=5;
+        int pos=1;
+        int bitMask=1<<pos;
+        if(oper==1){
+            int newNumber=bitMask | n;
+            System.out.println(newNumber);
+        }else{
+        int newBitMask=~(bitMask);
+        int newNumber=newBitMask &n;
+        System.out.println(newNumber);
+        }
+    }
+}
