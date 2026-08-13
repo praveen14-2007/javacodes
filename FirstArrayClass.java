c7495c966d011b16ae4d58c311981718c077cb8f
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:32:03 +0530

    Add FirstArrayClass to print student marks

---
+++ FirstArrayClass.java
@@ -0,0 +1,11 @@
+class FirstArrayClass{
+    public static void main(String[] args){
+        int[] marks=new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=95;
+        System.out.println(marks[0]); //for(int i=0;i<3;i++)
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
