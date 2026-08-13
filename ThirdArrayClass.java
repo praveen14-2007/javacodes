d8af0501f65f716148dea624920b6c738607b1af
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:33:27 +0530

    Implement ThirdArrayClass to find index of x

---
+++ ThirdArrayClass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ThirdArrayClass{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int x=sc.nextInt();
+        int numbers[]=new int[size];
+        for(int i=0;i<size;i++){
+            numbers[i]=sc.nextInt();
+        }
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]==x){
+                System.out.println("x is found at index:"+i);
+            }
+        }
+   }
+}
