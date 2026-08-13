4710450443e62a93eba93e1a9bc027d4c3d5c0f7
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:42:44 +0530

    Create EighthStringsClass with StringBuilder example
    Initial implementation of EighthStringsClass with StringBuilder usage.

---
+++ EighthStringsClass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class EighthStringsClass{
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("praveen");
+        System.out.println(sb);
+        System.out.println(sb.charAt(0));
+        //sb.setCharAt(0, 'r');
+        //sb.insert(2, 's');
+        //sb.delete(0,3);
+        //sb.append("kumar");
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
