ebe3cea0dbf9f4a73f4c6daf3010daacc096f407
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:38:24 +0530

    Create FirstStringsClass to concatenate two strings

---
+++ FirstStringsClass.java
@@ -0,0 +1,8 @@
+class FirstStringsClass{
+    public static void main(String[] args){
+        String name1="praveen";
+        String name2="kumar";
+        String name=name1 + name2;
+        System.out.println(name);
+    }
+}
