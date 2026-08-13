d47d25cbc4c53d8b63630241b79df868d0fc9c21
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:41:25 +0530

    Add SixthStringsClass to extract username from email

---
+++ SixthStringsClass.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class SixthStringsClass{
+   public static void main(String args[]) {
+     Scanner sc = new Scanner (System.in);
+     String email = sc.next();
+     String userName = "";
+     for(int i=0; i<email.length(); i++) {
+       if(email.charAt(i) == '@') {
+        break;
+       } else {
+         userName += email.charAt(i);
+       }
+     }
+     System.out.println(userName);
+   }
+}
