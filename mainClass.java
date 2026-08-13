d2f6450024238fdf4f7961d7ef3d5a06867716cf
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:07:57 +0530

    Add MainClass with main method and variables

---
+++ mainClass.java
@@ -0,0 +1,16 @@
+class MainClass{
+    public static void main(String[] args){
+        String name = "praveen";
+        int age =19;
+        double salary = 256943.25;
+        int a=450;
+        int b=456;
+        b=707;
+        name = "praveen kumar";
+        int sum = a+b;
+        System.out.println(sum);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
