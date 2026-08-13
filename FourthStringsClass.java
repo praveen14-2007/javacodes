bcbdb489faca5881e9147721fe2590569c7bb806
Praveen kumar <pk0173306@gmail.com>
2026-08-13 22:40:44 +0530

    Add FourthStringsClass to compare string equality

---
+++ FourthStringsClass.java
@@ -0,0 +1,12 @@
+class FourthStringsClass{
+    public static void main(String[] args){
+        //String name1="praveen";
+        //String name2="praveeen";
+        if(new String("pra")==new String("pra")){
+            System.out.println("Strings are equal");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
